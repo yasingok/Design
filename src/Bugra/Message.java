22fefd2e09981d85b695e0334d451a370b87049b
yasingok <yasingoktc@gmail.com>
2018-10-22 21:00:31 -0400

    Initial

---
+++ src/Bugra/Message.java
@@ -0,0 +1,9 @@
+package Bugra;
+
+public class Message {
+    public static void main(String[] args){
+
+        System.out.println("Ben Bugra Yasini Bugun Fifa da Acimasizca Yendigim Icin Tum Grup Arkadaslarimdan Ozur Dilerim");
+
+    }
+}
